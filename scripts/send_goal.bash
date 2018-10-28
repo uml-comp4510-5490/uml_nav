178c39504132905cee5435d59df0cef877ab8c60
James Kuczynski <james.perl12@gmail.com>
2018-10-27 22:58:00 -0400

    added launch scripts for Turtlebot

--- scripts/send_goal.bash
+++ scripts/send_goal.bash
@@ -13,6 +13,10 @@ case "$goalNum" in
         echo "sending goal 2"
         rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 1.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}}'
         ;;
+    3) 
+        echo "sending goal 2"
+        rostopic pub /move_base_simple/goal geometry_msgs/PoseStamped '{header: {stamp: now, frame_id: "map"}, pose: {position: {x: 1.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}}'
+        ;;
     *)
         echo "Invalid goal"
         ;;
